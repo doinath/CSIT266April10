96aed4f14638fcfeceeaca58530ee54e1edd37a5
LAB15_USER ON WS30 <L15Y13W30@citulab.net>
2025-04-10 14:19:17 +0800

    codes

--- dashboard.php
+++ dashboard.php
@@ -1,55 +1,125 @@
-
 <?php    
     include 'connect.php';
     include 'readrecords.php';   
     //require_once 'includes/header.php'; 
 ?>
-    
-<div>
-	<button><a href="register.php">Add New Student</a></button>
-</div> 
 
-<div>
+<style>
+    body {
+        font-family: Arial, sans-serif;
+        background-color: #fef9f4;
+        margin: 20px;
+    }
+
+    h2 {
+        color: white;
+        margin: 0;
+        padding: 10px 0;
+    }
+
+    .header {
+        background-color: #FAB12F;
+        text-align: center;
+        border-radius: 8px;
+        margin-bottom: 20px;
+    }
+
+    .button-group {
+        display: flex;
+        justify-content: flex-end;
+        gap: 10px;
+        margin-bottom: 15px;
+    }
+
+    .button-group button {
+        background-color: #FAB12F;
+        color: black;
+        padding: 8px 16px;
+        border: none;
+        border-radius: 6px;
+        cursor: pointer;
+        font-weight: bold;
+    }
+
+    .button-group button a {
+        text-decoration: none;
+        color: inherit;
+    }
+
+    table {
+        width: 100%;
+        max-width: 800px;
+        margin: 0 auto;
+        border-collapse: collapse;
+        background-color: #fff8dc;
+        box-shadow: 0 4px 8px rgba(0,0,0,0.1);
+        border-radius: 10px;
+        overflow: hidden;
+    }
+
+    thead {
+        background-color: #f5c842;
+    }
+
+    th, td {
+        padding: 14px;
+        text-align: center;
+        border-bottom: 1px solid #ccc;
+    }
+
+    tbody tr:hover {
+        background-color: #fff0b3;
+    }
+
+    .action-btns button {
+        background-color: #e0a800;
+        color: white;
+        padding: 6px 10px;
+        border: none;
+        border-radius: 4px;
+        margin: 0 2px;
+        cursor: pointer;
+    }
+
+    .action-btns button a {
+        color: white;
+        text-decoration: none;
+    }
+</style>
+
+<div class="button-group">
+	<button><a href="register.php">Add Vehicle</a></button>
 	<button><a href="logout.php">Logout</a></button>
 </div> 
 
+<div class="header">
+    <h2>List of Vehicles</h2>
+</div>
 
-<div style='background-color:#ffff00'>
-    <center>
-        <p style="color:white"><h2>List of Students</h2></p>
-    </center>
-</div>     
-
-<br>
-    <div>        
-        <table id="tblCustomerRecords " class="table
-            table-striped table-bordered table-sm" cellspacing="0" width="100%"> 
-            <thead>
-                <tr> 
-                    <th>ID Number</th> 
-                    <th>Firstname</th> 
-                    <th>Lastname</th>
-                    <th>Program</th>                     
-                    <th>Action</th>
-                </tr> 
-            </thead>  
-            <tbody>
-                <?php
-                    while($row = $resultset->fetch_assoc()):
-                    	$id = $row['uid'];
-                ?>
+<div>
+    <table>
+        <thead>
+            <tr> 
+                <th>License Plate</th>
+                <th>Vehicle Type</th>
+                <th>Vehicle Brand</th>
+                <th>Actions</th>
+            </tr> 
+        </thead>  
+        <tbody>
+            <?php while($row = $resultset->fetch_assoc()): ?>
                 <tr>
-                    <td><?php echo $id ?></td>
-                    <td><?php echo $row['firstname'] ?></td>
-                    <td><?php echo $row['lastname'] ?></td>
-                    <td><?php echo $row['program'] ?></td> 
-                    <td><button><a href="update.php">UPDATE</a></button> | <button><a href="delete.php">DELETE</a></button></td>
+                    <td><?php echo $row['license_plate']; ?></td>
+                    <td><?php echo $row['vehicle_type']; ?></td>
+                    <td><?php echo $row['vehicle_brand']; ?></td> 
+                    <td class="action-btns">
+                        <button><a href="update.php?license_plate=<?php echo $row['license_plate']; ?>">Update</a></button>
+                        <button><a href="delete.php?license_plate=<?php echo $row['license_plate']; ?>">Delete</a></button>
+                    </td>
                 </tr>
-                <?php endwhile;?>
-            </tbody>         
-        </table>
-        
-    </div>
+            <?php endwhile; ?>
+        </tbody>         
+    </table>
+</div>
 
 <?php //require_once 'includes/footer.php'; ?>
-
--- readrecords.php
+++ readrecords.php
@@ -6,12 +6,16 @@
 	    die('Could not connect: ' . mysqli_connect_error());
 }
 	
-	$query = 'SELECT * from  tblstudent, tbluser where tbluser.uid = tblstudent.uid';
+	$query = 'SELECT * from  tblvehicle; 
+	-- //tbluser where tbluser.uid = tblstudent.uid';
         $resultset = mysqli_query($connection, $query);
 	
-	//$querybsit = 'SELECT count(*) as total from  tblstudent where program = "BSIT"';
+	//$querybsit = 'SELECT count(*) as total from tblvehicle;
 	//$resultset1 = mysqli_query($connection, $querybsit);
 	//$count = mysqli_fetch_assoc($resultset1);	
+	// where  = "BSIT"';
+	// $resultset1 = mysqli_query($connection, $querybsit);
+	// $count = mysqli_fetch_assoc($resultset1);	
 		
 	
 ?>
